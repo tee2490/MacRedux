c5976543ae06691537ebdde153408d696eb9f1f8
Teeradet Tavarpinun <tee2490@hotmail.com>
2025-10-21 13:44:04 +0700

    Slice and Action

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigators from "./src/screen/Navigators";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
+        <Provider store={store}>
         <Navigators />
+        </Provider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
